b0e64b54348a10bebfe9e662de2faecbbc26ffa3
Iuliia Saprykina <iuliia.proskurnina@gmail.com>
2020-02-19 17:31:11 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,19 +10,18 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   #pp directors_database[0][:movies][0][:worldwide_gross]
   #pp directors_database
-  total = 0
   directors_totals = {}
   director_index = 0
-   while director_index < directors_database.length do
+   while director_index < nds.length do
+     total = 0
      inner_index = 0
-     movies = directors_database[director_index][:movies]
+     movies = nds[director_index][:movies]
     while inner_index < movies.length do
        total += movies[inner_index][:worldwide_gross]
-       result = {"#{directors_database[director_index][:name]}" => total}
        inner_index += 1 
      end
+     directors_totals[nds[director_index][:name]] = total
      director_index += 1
-     pp directors_database
   end
- return result
+ return directors_totals
 end
